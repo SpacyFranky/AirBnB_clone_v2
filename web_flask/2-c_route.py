e7b50b7413f6315d56bfa59492229f8362201d0f
spacyfranky <bigaziz@outlook.com>
2020-04-22 02:33:00 +0000

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application:
+   - web application must be listening on 0.0.0.0, port 5000
+   - /: displays "Hello HBNB!"
+   - /hbnb: displays "HBNB" """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """prints 'Hello HBNB!'
+    """
+    return('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints 'HBNB'
+    """
+    return('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """prints "C " followed by the value of the text variable
+       (replacing underscore _ symbols with a space)
+    """
+    text = text.replace('_', ' ')
+    return("C {}".format(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
